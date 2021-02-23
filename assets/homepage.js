9c3eda1387688f9ef9abb0a5c9695cf7684d7c1d
charJKL <unsignedJKL@gmail.com>
2021-02-23 11:29:10 +0100

    Add symfony/webpack-encore-bundle dependency. Refactor front-end to work with encore.

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,6 +1,13 @@
+import "./css/homepage.css";
+
+import strtotime from "locutus/php/datetime/strtotime.js";
+
+console.log(strtotime);
+console.log("works?");
+
 window.addEventListener("load", function(){
 	const calendar = document.getElementById("calendar");
-
+	console.log(calendar);
 	const months = [1,2];
 	months.forEach(function(e){
 		// TODO render caledar sheet 
--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
